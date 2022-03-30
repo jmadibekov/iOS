e16673795759746c3289a00a16128ec0cef69f59
Joe Madibekov <jmadibekov@gmail.com>
2022-03-30 22:43:09 +0600

    Rename some

---
+++ xcode/GettingStartedss/GettingStarted/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Nurbakyt on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var names = ["Tammy", "Cole"]
+        names.removeFirst()
+        names.removeFirst()
+    }
+
+
+}
+
---
+++ xcode/LearningXcodess/LearningXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningXcode
+//
+//  Created by Nurbakyt on 27.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
