c5655589881a837e6deef2b36a798573f4389758
Joe Madibekov <jmadibekov@gmail.com>
2022-05-03 15:08:45 +0600

    Change to programmatic segues in TrafficSegues

--- xcode/TrafficSegues/TrafficSegues/ViewController.swift
+++ xcode/TrafficSegues/TrafficSegues/ViewController.swift
@@ -8,20 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var textField: UITextField!
+    @IBOutlet var segueSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
-        
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = textField.text
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn {
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
     }
 }
 
