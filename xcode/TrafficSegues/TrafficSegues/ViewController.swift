4a91c2950ff5b03158fa7587f852e16f95309620
Joe Madibekov <jmadibekov@gmail.com>
2022-05-03 13:22:05 +0600

    Add new project TrafficSegues

---
+++ xcode/TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Nurbakyt on 03.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+        
+    }
+}
+
